1de42fe66940be06c65e0aad7bb5e156d666437f
joaovitor2019 <55900057+joaovitor2019@users.noreply.github.com>
2021-04-26 22:14:35 -0300

    parte 3 TS-1

--- TypeScript-1/01-alurabank/alurabank/app/ts/controllers/NegociacaoController.ts
+++ TypeScript-1/01-alurabank/alurabank/app/ts/controllers/NegociacaoController.ts
@@ -3,11 +3,15 @@ class NegociacaoController{
     private _inputData: HTMLInputElement;
     private _inputQuantidade: HTMLInputElement;
     private _inputValor: HTMLInputElement;
+    private _negociacoes = new Negociacoes();
+    private _negociacoesView = new NegociacoesView('#negociacoesView');
+    private _mensageView = new MensagemView('#mensagemView');
 
     constructor(){
         this._inputData = <HTMLInputElement>document.querySelector("#data");
         this._inputQuantidade = <HTMLInputElement>document.querySelector("#quantidade");
         this._inputValor = <HTMLInputElement>document.querySelector("#valor");
+        this._negociacoesView.update(this._negociacoes);
     }
 
     adiciona(event: Event){
@@ -20,7 +24,11 @@ class NegociacaoController{
             parseFloat(this._inputValor.value)
         );
 
-        console.log(negociacao);
+        this._negociacoes.adiciona(negociacao);
+
+        this._negociacoesView.update(this._negociacoes);
+
+        this._mensageView.update('Negociação adicionada com sucesso!!')
         
     }
 }
